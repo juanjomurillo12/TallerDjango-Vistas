63ff8bf6e5f19cb3d9d41b642237c4ba9208d585
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-02-08 20:11:33 -0500

    cambios

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "confort",
+        "USER": "juanjomurillo12",
+        "PASSWORD": "juanjo12",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
